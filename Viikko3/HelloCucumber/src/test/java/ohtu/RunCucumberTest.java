971d5883fb85946fd335f511d9ffbc0195d9d737
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-11-11 16:43:16 +0200

    Week 3, exercise 4, Hello Cucumber

---
+++ Viikko3/HelloCucumber/src/test/java/ohtu/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package ohtu;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = "pretty", 
+    features = "src/test/resources/ohtu", 
+    snippets = SnippetType.CAMELCASE 
+)
+
+public class RunCucumberTest {}
