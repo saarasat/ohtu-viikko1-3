b96e27500c907365c5bdd069d06bb2e8ce15ac92
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-11-21 19:47:20 +0200

    Week 4, exercises 1-2

---
+++ Viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -0,0 +1,59 @@
+
+package ohtu.matkakortti;
+
+import ohtu.matkakortti.Maksukortti;
+import ohtu.matkakortti.Kassapaate;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class KassapaateTest {
+    
+    Kassapaate kassa;
+    Maksukortti kortti;
+    
+    @Before
+    public void setUp() {
+        kassa = new Kassapaate();
+        kortti = mock(Maksukortti.class);
+    }
+    
+    @Test
+    public void kortiltaVelotetaanHintaJosRahaaOn() {
+        when(kortti.getSaldo()).thenReturn(10);
+        kassa.ostaLounas(kortti);
+        
+        verify(kortti, times(1)).getSaldo();
+        verify(kortti).osta(eq(Kassapaate.HINTA));
+    }
+
+    @Test
+    public void kortiltaEiVelotetaJosRahaEiRiita() {
+        when(kortti.getSaldo()).thenReturn(4);
+        kassa.ostaLounas(kortti);
+        
+        verify(kortti, times(1)).getSaldo();
+        verify(kortti, times(0)).osta(4);
+    }
+
+    @Test
+    public void lataaminenMaksukortilleOnnistuuJosSummaOnPositiivinen() {
+        kassa.lataa(kortti, 5);
+
+        verify(kortti).lataa(eq(5));
+    }
+
+    @Test
+    public void lataaminenMaksukortilleEiTeeMitäänJosSummaOnNegatiivinen() {
+        kassa.lataa(kortti, -5);
+
+        verify(kortti, times(0)).lataa(-5);
+    }
+
+      
+}
