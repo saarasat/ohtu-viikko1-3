3f4e85b0fc59b765e12d54ae7048d4820097d4cb
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-11-28 19:22:03 +0200

    Week 6, exercises 2,3

---
+++ Viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,50 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // seuraavassa osoitteessa 27.11.2019 päivitetyt tilastot
+        String url = "https://nhl27112019.herokuapp.com/players.txt";
+        // ajan tasalla olevat tilastot osoitteessa
+        // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+
+        Statistics stats = new Statistics(new PlayerReaderImpl(url));
+          
+        Matcher m = new And( new HasAtLeast(5, "goals"),
+                             new HasAtLeast(5, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+        System.out.println("");
+
+        Matcher n = new And( 
+            new Not( new HasAtLeast(1, "goals") ), 
+            new PlaysIn("NYR")
+        );
+
+        for (Player player : stats.matches(n)) {
+            System.out.println(player);
+        }
+        System.out.println("");
+
+
+        Matcher x = new And(
+            new HasAtLeast(20, "points"),
+            new Or( 
+                new PlaysIn("NYR"),
+                new PlaysIn("NYI"),
+                new PlaysIn("NJD")
+            )
+        );
+        
+        for (Player player : stats.matches(x)) {
+            System.out.println(player);
+        }
+        System.out.println("");
+
+    }
+}
